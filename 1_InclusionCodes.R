232b9beaecf5f8ceabe7b06275dc786daf05df3e
laurenrost <48293377+laurenrost@users.noreply.github.com>
2021-07-09 23:26:31 -0400

    Update 1_InclusionCodes.R

--- 1_InclusionCodes.R
+++ 1_InclusionCodes.R
@@ -5,25 +5,25 @@
 require("readxl")
 
 # Input inclusion icd-10 codes
-filepath <- "~/Desktop/DBMI/Research/Pharmacogenomics/Data/Final_R3_201005/Inclusion_icd10codes.v3.xlsx"
+filepath <- "~/Inclusion_icd10codes.xlsx"
 icl_icd10 <- read_excel(filepath)
 icl_icd10 <- as.matrix(icl_icd10)
 icl_icd10 <- gsub("-", "*", icl_icd10)
 
 # Input inclusion icd-9 codes
-filepath <- "~/Desktop/DBMI/Research/Pharmacogenomics/Data/Final_R3_201005/Inclusion_icd9codes.v3.xlsx"
+filepath <- "~/Inclusion_icd9codes.xlsx"
 icl_icd9 <- read_excel(filepath)
 icl_icd9 <- as.matrix(icl_icd9)
 icl_icd9[,1] <- paste0(icl_icd9[,1], "*")
 icl_icd9 <- icl_icd9[-15,] # Because there were none of these codes present in Diagnosis table ("311.00*" "Depressive disorder, not elsewhere classified" )
 
 # Input inclusion medications
-filepath <- "~/Desktop/DBMI/Research/Pharmacogenomics/Data/Final_R3_201005/Inclusion_Meds_R32027.v6.xlsx"
+filepath <- "~/Inclusion_Meds_R32027.v6.xlsx"
 incl_meds <- read_excel(filepath)
 incl_meds <- as.matrix(incl_meds)
 
 # Input diagnoses file to check for icd-9/10 codes in patients' records
-filepath <- "~/Desktop/DBMI/Research/Pharmacogenomics/Data/EHR/R3_2027_ROST_DIAGNOSES_2020_12_14.csv"
+filepath <- "~/DIAGNOSES.csv"
 t <- read.csv(filepath)
 
 
@@ -68,7 +68,7 @@ for (i in (2:(dim(incl_icd9_icd10)[2]))){
   
   # Write file iteratively
   incl_ppl_append <- incl_icd9_icd10[,1:i]
-  write.csv(incl_ppl_append,"~/Desktop/DBMI/Research/Pharmacogenomics/Data/PipelineResults/InclusionCodeResults_210201_3.csv", 
+  write.csv(incl_ppl_append,"~/InclusionCodeResults.csv", 
             row.names = FALSE, 
             quote=F,
             append=F)
